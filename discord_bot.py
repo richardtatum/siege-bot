a1f2124b69bba3a7ee1b7a5a832456a4077c73f1
Richard Tatum <Richardr@executivejet.local>
2018-09-18 14:35:23 +0100

    Embed code has been merged into main.

--- discord_bot.py
+++ discord_bot.py
@@ -1,7 +1,7 @@
 import discord
 import random
 import bs4
-from robobrowser import RoboBrowser
+import requests
 from settings import users
 
 TOKEN = 'NDkwMTIyNjY1MzM2NTA0MzIy.Dn0uGQ.uokwSV1FgO39Id7exalxEB2AvE0'
@@ -9,11 +9,14 @@ TOKEN = 'NDkwMTIyNjY1MzM2NTA0MzIy.Dn0uGQ.uokwSV1FgO39Id7exalxEB2AvE0'
 client = discord.Client()
 
 
-def data_request(author, id):
-    br = RoboBrowser()
-    br.open('https://r6stats.com/stats/{}/'.format(id))
-    r = str(br.parsed)
+def data_request(message, author, id):
+    # global casual
+    # global waifu
+    # global waifupicture
+    # global profileurl
+    # global username_web
 
+    r = requests.get('https://r6stats.com/stats/{}/'.format(id)).text
     scrape = bs4.BeautifulSoup(r, 'html.parser')
 
     label = []
@@ -28,7 +31,6 @@ def data_request(author, id):
                     stats_type.get_text() == 'Ranked Stats':
                 cas_rank.append(stats_type.get_text())
         for card in element.select('.card__content'):
-            # print(card)
             for row in card.select('.desktop-only'):
                 for text in row.select('.stat-label'):
                     label.append(text.get_text())
@@ -37,37 +39,49 @@ def data_request(author, id):
                     count.append(text.get_text())
             data.append(dict(zip(label, count)))
 
-    for user in scrape.select('.username'):
-        user = user.get_text()
+    # for user in scrape.select('.username'):
+    #     username_web = user.get_text()
 
     for mostplayed in scrape.select('.player-header__left-side'):
         waifu = str(mostplayed)
+        waifupicture = str(waifu[waifu.find('src="')+5:waifu.find('"/><')])
         waifu = str(waifu[waifu.find('t="')+3:waifu.find('" src=')])
 
+    for profile in scrape.select('.profile'):
+        profile = str(profile)
+        profileurl = str(profile[profile.find('src="')+5:profile.find('"/>')])
+
     total.append(dict(zip(cas_rank, data)))
-    ranked = total[0]['Ranked Stats']
-    casual = total[0]['Casual Stats']
 
-    if casual is not None:
-        if user.lower() == author.lower():
-            msg = 'Standby for kill data!'
-            msg2 = 'User {} has the following stats: \n \
-                                   Time played: {} \n \
-                                   Kills: {} \n \
-                                   Deaths: {} \n \
-                                   K/D: {} \n \
-                                   W/L: {} \n \
-                                   Waifu: {}'.format(author, casual['Time Played'], casual['Kills'], casual['Deaths'], casual['K/D Ratio'], casual['W/L Ratio'], waifu)
-
-            return msg, msg2
-
-        else:
-            msg = 'I can\'t access the R6Stats site right now so I\'ll just guess your K/D instead.'
-            msg2 = 'User {} has a KD of {} on Rainbow 6: Siege'.format(author, round(random.uniform(1, 5), 3))
-            return msg, msg2
+    try:
+        casual = total[0]['Casual Stats']
+        kd = round(float(casual['K/D Ratio']), 3)
+        wl = round(float(casual['W/L Ratio']), 3)
+        return embed_creator(message, author, profileurl, casual['Time Played'], casual['Kills'], casual['Deaths'],
+                             kd, wl, waifu, waifupicture)
+    except:
+        return error_message(message, author)
 
 
+async def error_message(message, author):
+    msg = 'I can\'t access the R6Stats site right now so I\'ll just guess your K/D instead.'
+    msg2 = 'User {} has a KD of {} on Rainbow 6: Siege'.format(author, round(random.uniform(1, 5), 3))
+    await client.send_message(message.channel, msg)
+    await client.send_message(message.channel, msg2)
 
+
+async def embed_creator(message, username, profileurl, timeplayed, kills, deaths, kd, wl, waifu, waifupicture):
+    embed=discord.Embed(title="Username", description=username)
+    embed.set_thumbnail(url=profileurl)
+    embed.add_field(name="Time Played", value=timeplayed, inline=True)
+    embed.add_field(name="Kills", value=kills, inline=True)
+    embed.add_field(name="Deaths", value=deaths, inline=True)
+    embed.add_field(name="K/D Ratio", value=kd, inline=True)
+    embed.add_field(name="W/L Ratio", value=wl, inline=True)
+    embed.add_field(name="Waifu", value=waifu, inline=True)
+    embed.set_image(url=waifupicture)
+    embed.set_footer(text="*Is there something wrong with this bot? Please let us know by emailing tough.shit@codeishard.com.*")
+    await client.send_message(message.channel, embed=embed)
 
 @client.event
 async def on_message(message):
@@ -80,7 +94,7 @@ async def on_message(message):
         await client.send_message(message.channel, msg)
 
     if message.content.startswith('!insult'):
-        msg = 'What the fuck is wrong with you {0.author.mention}?'.format(message)
+        msg = 'What the *fuck* is wrong with you {0.author.mention}?'.format(message)
         await client.send_message(message.channel, msg)
 
     if message.content.startswith('!stats'):
@@ -95,13 +109,13 @@ async def on_message(message):
         print('>Stats check by ' + str(message.author))
         u = str(message.author)
         if u in users:
+            username_local = users[u][0]
             id = users[u][2]
-            user = users[u][0]
-            output = data_request(user, id)
-            msg = output[0]
-            msg2 = output[1]
+            await data_request(message, username_local, id)
+        else:
+            msg = 'I\'m afraid I don\'t have your ID stored for Rainbow 6. Please speak to Richard to get you added to the list.'
             await client.send_message(message.channel, msg)
-            await client.send_message(message.channel, msg2)
+
 
 
 @client.event
