cd15fd5c1a7bdbf099ac678ebe0ad0d8baac2cb5
Richard Tatum <Richardr@executivejet.local>
2018-09-18 13:16:34 +0100

    Added username check before embed.
    Code is still spaghetti.

--- discord_bot_embed_test.py
+++ discord_bot_embed_test.py
@@ -14,6 +14,7 @@ def data_request(author, id):
     global waifu
     global waifupicture
     global profileurl
+    global username_web
 
     br = RoboBrowser()
     br.open('https://r6stats.com/stats/{}/'.format(id))
@@ -43,13 +44,12 @@ def data_request(author, id):
             data.append(dict(zip(label, count)))
 
     for user in scrape.select('.username'):
-        user = user.get_text()
+        username_web = user.get_text()
 
     for mostplayed in scrape.select('.player-header__left-side'):
         waifu = str(mostplayed)
         waifupicture = str(waifu[waifu.find('src="')+5:waifu.find('"/><')])
         waifu = str(waifu[waifu.find('t="')+3:waifu.find('" src=')])
-        print(waifupicture)
 
     for profile in scrape.select('.profile'):
         profile = str(profile)
@@ -58,38 +58,6 @@ def data_request(author, id):
     total.append(dict(zip(cas_rank, data)))
     ranked = total[0]['Ranked Stats']
     casual = total[0]['Casual Stats']
-
-
-    # embed=discord.Embed(title="R6 Stats", description="User {} has the following stats".format(user))
-    # embed.set_thumbnail(url=waifupicture)
-    # embed.add_field(name="Time Played", value=casual['Time Played'], inline=True)
-    # embed.add_field(name="Kills", value=casual['Kills'], inline=True)
-    # embed.add_field(name="Deaths", value=casual['Deaths'], inline=True)
-    # embed.add_field(name="K/D Ratio", value=casual['K/D Ratio'], inline=True)
-    # embed.add_field(name="W/L Ratio", value=casual['W/L Ratio'], inline=True)
-    # embed.add_field(name="Waifu", value=waifu, inline=True)
-    # await self.bot.say(embed=embed)
-
-
-    if casual is not None:
-        if user.lower() == author.lower():
-            msg = 'Standby for kill data!'
-            msg2 = 'User {} has the following stats: \n \
-                                   Time played: {} \n \
-                                   Kills: {} \n \
-                                   Deaths: {} \n \
-                                   K/D: {} \n \
-                                   W/L: {} \n \
-                                   Waifu: {}'.format(author, casual['Time Played'], casual['Kills'], casual['Deaths'], casual['K/D Ratio'], casual['W/L Ratio'], waifu)
-
-            return msg, msg2
-
-        else:
-            msg = 'I can\'t access the R6Stats site right now so I\'ll just guess your K/D instead.'
-            msg2 = 'User {} has a KD of {} on Rainbow 6: Siege'.format(author, round(random.uniform(1, 5), 3))
-            return msg, msg2
-
-
 
 
 @client.event
@@ -118,19 +86,24 @@ async def on_message(message):
         print('>Stats check by ' + str(message.author))
         u = str(message.author)
         if u in users:
-            user = users[u][0]
+            username_local = users[u][0]
             id = users[u][2]
-            output = data_request(user, id)
-            embed=discord.Embed(title="R6 Stats", description="User {} has the following stats".format(user))
-            embed.set_thumbnail(url=profileurl)
-            embed.add_field(name="Time Played", value=casual['Time Played'], inline=False)
-            embed.add_field(name="Kills", value=casual['Kills'], inline=False)
-            embed.add_field(name="Deaths", value=casual['Deaths'], inline=False)
-            embed.add_field(name="K/D Ratio", value=casual['K/D Ratio'], inline=False)
-            embed.add_field(name="W/L Ratio", value=casual['W/L Ratio'], inline=False)
-            embed.add_field(name="Waifu", value=waifu, inline=True)
-            embed.set_image(url=waifupicture)
-            await client.send_message(message.channel, embed=embed)
+            data_request(username_local, id)
+            if username_local.lower() == username_web.lower():
+                embed=discord.Embed(title="R6 Stats", description="User {} has the following stats".format(username_local))
+                embed.set_thumbnail(url=profileurl)
+                embed.add_field(name="Time Played", value=casual['Time Played'], inline=False)
+                embed.add_field(name="Kills", value=casual['Kills'], inline=False)
+                embed.add_field(name="Deaths", value=casual['Deaths'], inline=False)
+                embed.add_field(name="K/D Ratio", value=casual['K/D Ratio'], inline=False)
+                embed.add_field(name="W/L Ratio", value=casual['W/L Ratio'], inline=False)
+                embed.add_field(name="Waifu", value=waifu, inline=True)
+                embed.set_image(url=waifupicture)
+                await client.send_message(message.channel, embed=embed)
+            else:
+                msg = 'I can\'t access the R6Stats site right now so I\'ll just guess your K/D instead.'
+                msg2 = 'User {} has a KD of {} on Rainbow 6: Siege'.format(author, round(random.uniform(1, 5), 3))
+                return msg, msg2
 
 
 @client.event
