6c81248bf284876880d6d25e541af936e61dbb6b
Josh Edney <josh.edney@blubolt.com>
2018-09-20 16:19:11 +0100

    Test comment

--- server_scripts/git-update.sh
+++ server_scripts/git-update.sh
@@ -10,11 +10,13 @@ if [[ ! -d /opt/r6stats-discord-bot/log/ ]]; then
 	mkdir /opt/r6stats-discord-bot/log
 fi
 
+# Old version
 OLD_HEAD=`git rev-parse HEAD`
 
 # Do a git pull with the correct key and log the response.
 ssh-agent bash -c 'ssh-add /opt/id_rsa; OUTCOME=$(git pull); TIMESTAMP=$(date); echo ${TIMESTAMP} "-" ${OUTCOME}' >> /opt/r6stats-discord-bot/log/update.log
 
+# New version
 NEW_HEAD=`git rev-parse HEAD`
 
 # Check to see if it is newer.
