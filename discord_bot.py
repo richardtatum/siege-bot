c2fd3ee8ec0325b6c3d7767624c2ff02e423bebb
Richard Tatum <Richardr@executivejet.local>
2018-09-25 14:56:38 +0100

    Updated to take stats from r6.tracker.net
    This means stats are updated now every 3 minutes instead of once an hour.

--- discord_bot.py
+++ discord_bot.py
@@ -25,10 +25,11 @@ TOKEN = 'NDkwMTIyNjY1MzM2NTA0MzIy.Dn0uGQ.uokwSV1FgO39Id7exalxEB2AvE0'
 
 
 # Webscraper function, with required arguments passed from the call.
-def data_request(context, casual_ranked, author, id):
+def data_request(context, casual_ranked, author):
     # Here the unique identifier is input into the URL to get the correct page
-    r = requests.get('https://r6stats.com/stats/{}/'.format(id)).text
-    scrape = bs4.BeautifulSoup(r, 'html.parser')
+    # r = requests.get('https://r6.tracker.network/profile/pc/sharkie668')
+    r = requests.get('https://r6.tracker.network/profile/pc/{}'.format(author.lower()))
+    scrape = bs4.BeautifulSoup(r.text, 'html.parser')
 
     label = []  # Blank list for the stat labels
     count = []  # Blank list for the stat numbers
@@ -38,75 +39,63 @@ def data_request(context, casual_ranked, author, id):
 
     # Webscraper passes through classes picking up the correct information
     # And assigns them to the blank lists for manipulation later.
-    for element in scrape.select('.stats-center'):
-        for stats_type in element.select('.card__title'):
-            if stats_type.get_text() == 'Casual Stats' or \
-                    stats_type.get_text() == 'Ranked Stats':
-                cas_rank.append(stats_type.get_text())
-        for card in element.select('.card__content'):
-            for row in card.select('.desktop-only'):
-                for text in row.select('.stat-label'):
-                    label.append(text.get_text())
+    for element in scrape.select('.r6-pvp-grid'):
+        for c_r in element.select('.trn-card__header'):
+            cas_rank.append(c_r.get_text(strip=True))
 
-                for text in row.select('.stat-count'):
-                    count.append(text.get_text())
+        for stats_type in element.select('.trn-card__content'):
+            for stats_list in stats_type.select('.trn-defstats'):
+                for stat_label in stats_list.select('.trn-defstat__name'):
+                    label.append(stat_label.get_text(strip=True))
+                for stat_count in stats_list.select('.trn-defstat__value'):
+                    count.append(stat_count.get_text(strip=True))
+                    # strip=True removes the '\n' new lines present in the list
             data.append(dict(zip(label, count)))
 
     # Takes all the data and zips into one easy to use dict.
     total.append(dict(zip(cas_rank, data)))
 
-    # Grabs the username from the website, for use in checking against the
-    # stored username taken earlier. This helps prevent a bug where the website
-    # would display someone elses information if the ID was unaccessable.
-    for user in scrape.select('.username'):
-        username_web = user.get_text()
+    # Pulls the users uPlay profile image
+    for profile in scrape.select('.trn-profile-header__avatar'):
+        for link in profile.find_all('img', src=True):
+            link = str(link)
+            # Strips the string of unnecessary characters and leaves the image url
+            profileurl = str(link[link.find('src="')+5:link.find('"/>')])
 
     # Pulls the name of the most played character from inside an image link.
-    for mostplayed in scrape.select('.player-header__left-side'):
-        waifu = str(mostplayed)
-
-        # Strips the string of unnecessary characters and leaves the image link
-        waifupicture = str(waifu[waifu.find('src="')+5:waifu.find('"/><')])
-
-        # Strips the string of unnecessary characters and leaves the Operator name
-        waifu = str(waifu[waifu.find('t="')+3:waifu.find('" src=')])
-
-    # Pulls the users uPlay profile image
-    for profile in scrape.select('.profile'):
-        profile = str(profile)
-
-        # Again strips unnecessary characters and leaves the image link
-        profileurl = str(profile[profile.find('src="')+5:profile.find('"/>')])
+    for mostplayed in scrape.select('.trn-defstat__value'):
+        for source in mostplayed.find_all('img', src=True, limit=1):
+            source = str(source)
+            # Strips the string of unnecessary characters and leaves the Operator name
+            waifu = str(source[source.find('title="')+7:source.find('"/>')]).title()
+            # Takes the operator name and adds it to a URL to pull a picture of that Op
+            waifupicture = 'https://cdn.r6stats.com/figures/{}_figure.png'.format(waifu.lower())
 
     try:
-        # Checks the username scraped from the website against the locally stored
-        # name in settings.py to make sure we aren't posting someone elses details
-        if author.lower() == username_web.lower():
-            # If the usernames are the same, pick the casual stats from the total dict
-            stats_CR = total[0]['{} Stats'.format(casual_ranked.title())]
+        # Pulls the request for casual or ranked stats. Defaulting to casual
+        # if no argument is provided
+        requested_cas_rank = total[0]['{}'.format(casual_ranked.title())]
+        # If the user has not played ranked/casual then the 'Time Played' stat
+        # is blank. This checks the length and makes it 0 if nothing is there
+        if len(requested_cas_rank['Time Played']) == 0:
+            requested_cas_rank['Time Played'] = 0
 
-            # Rounds the K/D & W/L numbers to 2 decimal points for easy digestion
-            kd = round(float(stats_CR['K/D Ratio']), 2)
-            wl = round(float(stats_CR['W/L Ratio']), 2)
-
-            # Passes all information to the embed_creator for message creation.
-            return embed_creator(context, casual_ranked, author, profileurl,
-                                 stats_CR['Time Played'], stats_CR['Kills'],
-                                 stats_CR['Deaths'], kd, wl, waifu,
-                                 waifupicture)
-        else:
-            # If the usernames don't match, return an error message
-            return error_message(context, author)
+        # Passes all information to the embed_creator for message creation.
+        return embed_creator(context, casual_ranked, author, profileurl,
+                             requested_cas_rank['Time Played'], requested_cas_rank['Kills'],
+                             requested_cas_rank['Deaths'], requested_cas_rank['KD'],
+                             requested_cas_rank['Win %'], waifu, waifupicture)
 
     except:
         # If the webscraper hasn't collect the correct information, return error.
+        print('>Check failed. Make sure username is correct?')
         return error_message(context, author)
 
 
 # A fun way of distracting the user if the webscrape fails.
 # round(random) function used to pick a float between 1-5 and round to 2 decimal points.
 async def error_message(context, author):
-    msg = 'I can\'t access the R6Stats site right now so I\'ll just guess your K/D instead.'
+    msg = 'I can\'t access the R6Tracker site right now so I\'ll just guess your K/D instead.'
     msg2 = 'User {} has a KD of {} on Rainbow 6: Siege'.format(author, round(random.uniform(1, 5), 2))
     await client.send_message(context.message.channel, msg)
     await client.send_message(context.message.channel, msg2)
@@ -122,10 +111,11 @@ async def embed_creator(context, casual_ranked, username, profileurl, timeplayed
     embed.add_field(name="Kills", value=kills, inline=True)
     embed.add_field(name="Deaths", value=deaths, inline=True)
     embed.add_field(name="K/D Ratio", value=kd, inline=True)
-    embed.add_field(name="W/L Ratio", value=wl, inline=True)
+    embed.add_field(name="W/L %", value=wl, inline=True)
     embed.add_field(name="Waifu", value=waifu, inline=False)
     embed.set_image(url=waifupicture)
-    embed.set_footer(text="*Is there something wrong with this bot? Please let us know by emailing tough.shit@codeishard.com.*")
+    embed.set_footer(text="*Is there something wrong with this bot? Please let us know by \
+    emailing tough.shit@codeishard.com.*")
     await client.send_message(context.message.channel, embed=embed)
 
 
@@ -156,17 +146,17 @@ async def r6(context, casual_ranked='casual'):
     # Turns the author name into a string so it can be checked against the list.
     u = str(context.message.author)
     # Console print for the log, so you can see who has triggered the bot.
-    print('>Stats check by user ' + u)
+    print('>Stats check by user ' + u + ' | ' + casual_ranked.title())
     # Checks if the argument called is either of the accepted ones.
     if casual_ranked.lower() == 'casual' or casual_ranked.lower() == 'ranked':
         # If the author is present in the list, continue with codeself.
         # Otherwise this author hasn't been created. Prompt contact with admin.
         if u in users:
             username_local = users[u][0]  # Username_local stored for checking later
-            id = users[u][2]  # ID of the account, will be parsed into the URL later
             # Pass this information to the data_request() func.
-            await data_request(context, casual_ranked, username_local, id)
+            await data_request(context, casual_ranked, username_local)
         else:
+            print('>Check failed. Is the username on the list?')
             msg = 'I\'m afraid I don\'t have your ID stored for Rainbow 6. Please speak to Richard to get you added to the list.'
             await client.say(msg)
     # If the argument is not one of the two accepted, throws a helpful error message.
