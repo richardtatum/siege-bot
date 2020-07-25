d97b483ee4ecaa4f8ad04238340633b9a0f9b5fc
Richard Tatum <rtatum@pm.me>
2020-07-25 15:26:03 +0100

    basic settings file for holding config info

---
+++ settings.py
@@ -0,0 +1,6 @@
+# This is the bot token from the Discord dev page
+TOKEN = "add_server_token_here"
+CHAL_ICON = "https://cdn.r6.leaderboards.io/images/challenge_icons/RB6_Placeholder_Event.png"
+API_AUTH = "insert_api_auth_here"
+API_URL = "https://api.r6.leaderboards.io"
+
